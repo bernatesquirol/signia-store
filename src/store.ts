eb06ec8253657f44e2ff247a552b378a903aaade
bernat <bernat@bestiario.org>
2023-06-13 10:07:32 +0200

    nova versio

--- src/store.ts
+++ src/store.ts
@@ -4,6 +4,7 @@ export function get(key, ctx='store'){
   if (!globalThis[ctx]){
     globalThis[ctx] = {}
   }
+  // console.log(globalThis[ctx][key])
   return globalThis[ctx][key]
 }
 export function setSingle(key, atom, ctx: string='store'){
@@ -37,25 +38,57 @@ const subsAll = (stringFunc, transformMatch)=>{
   }
   return stringFunc
 }
+function getSelectedCtx(key,ctxs=[]){
+  if (!Array.isArray(ctxs)){
+     ctxs = [ctxs]
+    }
+  let filtered = ctxs.filter(ctx=>globalThis[ctx] && globalThis[ctx][key])
+  if (filtered.length>0){
+    if (filtered.length>1) console.log('STORE', 'multiple context found', key)
+    const selectedCtx = filtered[filtered.length-1]
+    return selectedCtx
+  }else{
+    console.log('STORE', 'no context found', key)
+  }
+  return null
+}
+const getArgs = (func)=>{
+  let str = func.toString();
+  let args = /\(\s*([^)]+?)\s*\)/.exec(str);
+  if (args && args[1]) {
+    return args[1].split(/\s*,\s*/);
+  }
+  return []
+}
 // type a = Parameters<typeof computedSignia<any,any>>
-export function computed<T,K>(key, func, ctxs=['store'], ctxWrite: string|string[]="store") {
-  if (!Array.isArray(ctxs)) ctxs = [ctxs]
-  // globalThis[key] = computedSignia<T,K>(key, func)
-  let stringFunc = func.toString()
+export function computed<T,K>(key, func, ctxs=['store'], ctxWrite: string|string[]="store", aliases={},specificContexts={}, ) {
   const transformMatch = (match)=>{
-    let filtered = ctxs.filter(ctx=>globalThis[ctx] && globalThis[ctx][match])
-    if (filtered.length>0){
-      if (filtered.length>1) console.log('STORE', 'multiple context found', match)
-      const selectedCtx = filtered[0]
-      return `globalThis["${selectedCtx}"]["${match}"].value`
-    }else{
-      console.log('STORE', 'no context found', match)
+    if (aliases[match]){
+      match = aliases[match]
+    }
+    let specific = specificContexts[match]    
+    let selectedCtx = specific?specific:getSelectedCtx(match, ctxs)
+    if (selectedCtx!=null){
+      return globalThis[selectedCtx][match]
     }
     return null
   }
-  let stringFuncNew = subsAll(stringFunc, transformMatch) // match=>`globalThis["${ctx}"]["${match}"].value`
-  let newFunc = new Function("return "+stringFuncNew)()
+  // let args = getArgs(func)
+  // let values = args.map(transformMatch)
+  let newFunc = ()=>{
+    const getter = (m)=>transformMatch(m)?.value
+    return func(getter)
+  }
   let atomComputed = computedSignia<T,K>(key, newFunc)
   set(key, atomComputed, ctxWrite)
   return atomComputed
+}
+export function getAtoms(keys, ctxs=['store']){
+  if (!Array.isArray(keys)) keys = [keys]
+  if (!Array.isArray(ctxs)) ctxs = [ctxs]
+  return Object.fromEntries(keys.map(key=>{
+    const selectedCtx = getSelectedCtx(key, ctxs)
+    const atom = get(key, selectedCtx)
+    return [`${key}Atom`, atom]
+  }))
 }
